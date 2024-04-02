5669f29d502aa0d228626fe39ba34383b485cf28
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-04-02 03:05:45 +0100

    edit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./styles.css"
 
-export default function Navbar(props) {
-
-    const {darkBackground} = props
-
-       const styles = {
-        backgroundColor: darkBackground ? "#ffffff" : "#00000",
+export default function Navbar() {
+    const [darkBackground, setDarkBackground] = React.useState(true) 
+    
+    const styles = {
+        backgroundColor: darkBackground ? "#fff" : "#000",
         color: darkBackground ? "#000000" : "#ffffff",
         borderColor: darkBackground ? "#000000" : "#ffffff"
-}
+     
+    }
+    
+    function handleClick() {
+        setDarkBackground(prevDarkBackground => {
+            return !prevDarkBackground
+        })
+
+      
+
+    }
+
+
     return <>
         <div style={styles} className="navbar">
             <div className="logo">
@@ -18,9 +29,9 @@ export default function Navbar(props) {
             </div>
 
             <div className="backgPicker">
-            <p>Light</p>
-            <div className="toggleSwitch"></div>
-            <p>Dark</p>
+            <p className={darkBackground? "#000": "dark"} >Light</p>
+            <div onClick={handleClick} className="toggleSwitch"></div>
+            <p className={darkBackground? "dark": "light"} >Dark</p>
             </div>
             
         </div>
